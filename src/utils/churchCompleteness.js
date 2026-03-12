3649d19bb737a2cb740b0800bc23ecf3d92311dc
Will Morrow <65681991+morrowwc@users.noreply.github.com>
2026-03-12 16:17:46 -0500

    Update churchCompleteness.js

--- src/utils/churchCompleteness.js
+++ src/utils/churchCompleteness.js
@@ -1,6 +1,7 @@
 const REQUIRED_CHURCH_FIELDS = [
   { key: "church_name", label: "name" },
   { key: "church_physical_city", label: "city" },
+  { key: "church_mailing_address", label: "mailing address" },
   { key: "church_physical_state", label: "state" },
   { key: "church_physical_county", label: "county" },
   { key: "church_phone_number", label: "phone number" },
