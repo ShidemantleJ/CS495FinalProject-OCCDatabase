af88b60174462e0ad0a25215dabf837416dde447
BradleyJHicks <bradleyjhicks04@gmail.com>
2026-04-02 17:07:13 -0500

    fixed nav

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import AddChurch from "./pages/addChurch";
 import EditChurch from "./pages/editChurch";
 import TeamMember from "./pages/teamMember";
 import Individuals from "./pages/individuals";
+import FormSubmissions from "./pages/formSubmissions";
 import EditTemplates from "./pages/editTemplates";
 import AddIndividual from "./pages/addIndividual";
 import ForgotPassword from "./pages/forgotPassword";
@@ -73,6 +74,7 @@ function App() {
                     <Route path="/team-members" element={<TeamMembers />} />
                     <Route path="/team-member/:id" element={<TeamMember />} />
                     <Route path="/individuals" element={<Individuals />} />
+                    <Route path="/form-submissions" element={<FormSubmissions />} />
                     <Route path="/edit-templates" element={<EditTemplates />} />
                    
 
